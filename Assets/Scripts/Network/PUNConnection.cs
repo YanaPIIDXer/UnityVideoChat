86174895303d9112b585f6bbfea41d2bb8d054a4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-30 20:27:11 +0900

    とりあえず接続処理

---
+++ Assets/Scripts/Network/PUNConnection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pun2Task;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using System;
+
+namespace VideoChat.Network
+{
+    /// <summary>
+    /// PUNとの接続
+    /// </summary>
+    public class PUNConnection : MonoBehaviour
+    {
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static PUNConnection Instance { get; private set; } = null;
+
+        /// <summary>
+        /// サーバ接続時Subject
+        /// </summary>
+        private Subject<Unit> OnConnectedServerSubject = new Subject<Unit>();
+
+        /// <summary>
+        /// サーバに接続した
+        /// </summary>
+        public Subject<Unit> OnConnectedServer { get { return OnConnectedServerSubject; } }
+
+        async void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            try
+            {
+                var Token = this.GetCancellationTokenOnDestroy();
+                await Pun2TaskNetwork.ConnectUsingSettingsAsync(Token);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Connect to PUN Failed. Reason:" + e.Message);
+            }
+
+            OnConnectedServerSubject.OnNext(Unit.Default);
+            OnConnectedServerSubject.OnCompleted();
+        }
+    }
+}
