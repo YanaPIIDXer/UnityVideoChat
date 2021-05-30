c2869c0b177b45c341022deb65233aac4953047f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-31 03:35:11 +0900

    カメライメージ

---
+++ Assets/Scripts/UI/CameraImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VideoChat.UI
+{
+    /// <summary>
+    /// カメライメージ
+    /// </summary>
+    public class CameraImage : MonoBehaviour
+    {
+    }
+}
