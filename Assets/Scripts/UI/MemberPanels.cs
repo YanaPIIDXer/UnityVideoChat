7f1676f8dae4959a8faebbc194905c7a07a12c6a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-31 04:05:56 +0900

    他人をセットするメソッド定義

--- Assets/Scripts/UI/MemberPanels.cs
+++ Assets/Scripts/UI/MemberPanels.cs
@@ -22,12 +22,27 @@ namespace VideoChat.UI
         private RectTransform[] OtherPanel = new RectTransform[4] { null, null, null, null };
 
         /// <summary>
+        /// 現在の他人の数
+        /// </summary>
+        private int CurrentOtherCount = 0;
+
+        /// <summary>
         /// 自分自身のパネルにセット
         /// </summary>
         /// <param name="OwnTransform">Transform</param>
         public void SetOwn(RectTransform OwnTransform)
         {
             OwnTransform.SetParent(OwnPanel, false);
+        }
+
+        /// <summary>
+        /// 他人をセット
+        /// </summary>
+        /// <param name="OtherTransform">他人のTransform</param>
+        public void SetOther(RectTransform OtherTransform)
+        {
+            OtherTransform.SetParent(OtherPanel[CurrentOtherCount], false);
+            CurrentOtherCount++;
         }
     }
 }
