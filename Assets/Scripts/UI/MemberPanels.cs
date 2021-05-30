a1cc3a1ed1f22973b3c8d64e4309e98487e5b586
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-31 03:28:56 +0900

    メンバの画面表示用パネルを追加

---
+++ Assets/Scripts/UI/MemberPanels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VideoChat.UI
+{
+    /// <summary>
+    /// メンバ表示用パネル
+    /// </summary>
+    public class MemberPanels : MonoBehaviour
+    {
+        /// <summary>
+        /// 自分自身のパネル
+        /// </summary>
+        [SerializeField]
+        private GameObject OwnPanel = null;
+
+        /// <summary>
+        /// 他人のパネル
+        /// </summary>
+        [SerializeField]
+        private GameObject[] OtherPanel = new GameObject[4] { null, null, null, null };
+    }
+}
