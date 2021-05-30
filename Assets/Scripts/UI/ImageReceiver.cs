f62b647ed2a2a255eccc8f6683d20c9e13d95b38
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-31 05:49:06 +0900

    ハンドシェイク送受信処理

--- Assets/Scripts/UI/ImageReceiver.cs
+++ Assets/Scripts/UI/ImageReceiver.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 namespace VideoChat.UI
 {
     /// <summary>
     /// 映像受信
     /// </summary>
-    public class ImageReceiver : MonoBehaviour, IPunInstantiateMagicCallback
+    public class ImageReceiver : MonoBehaviour, IPunInstantiateMagicCallback, IOnEventCallback
     {
         /// <summary>
         /// PhotonView
@@ -18,6 +20,16 @@ namespace VideoChat.UI
         void Awake()
         {
             View = GetComponent<PhotonView>();
+        }
+
+        void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        void OnDisable()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
         }
 
         public void OnPhotonInstantiate(PhotonMessageInfo info)
@@ -30,6 +42,31 @@ namespace VideoChat.UI
             {
                 MemberPanels.Instance.SetOther(GetComponent<RectTransform>());
             }
+        }
+
+        public void OnEvent(EventData photonEvent)
+        {
+            switch (photonEvent.Code)
+            {
+                case ImageSender.HandshakeEventCode:
+
+                    int[] Data = photonEvent.CustomData as int[];
+                    OnRecvHandshake(Data[0], Data[1], Data[2], Data[3]);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// ハンドシェイクを受信した
+        /// </summary>
+        /// <param name="ViewID">ViewID</param>
+        /// <param name="Width">幅</param>
+        /// <param name="Height">高さ</param>
+        /// <param name="DataLength">データ長</param>
+        private void OnRecvHandshake(int ViewID, int Width, int Height, int DataLength)
+        {
+            Debug.Log("OnRecvHandshake");
+            Debug.Log("ViewID:" + ViewID + " Width:" + Width + " Height:" + Height + " DataLength:" + DataLength);
         }
     }
 }
--- Assets/Scripts/UI/ImageSender.cs
+++ Assets/Scripts/UI/ImageSender.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
 
 namespace VideoChat.UI
 {
@@ -20,14 +25,54 @@ namespace VideoChat.UI
         /// </summary>
         private CameraImage CamImage = null;
 
+        /// <summary>
+        /// ハンドシェイクのイベントコード
+        /// </summary>
+        public const byte HandshakeEventCode = 10;
+
+        /// <summary>
+        /// ストリームのイベントコード
+        /// </summary>
+        public const byte StreamEventCode = 11;
+
         void Awake()
         {
             View = GetComponent<PhotonView>();
             CamImage = new CameraImage();
 
-            // テスト
-            GetComponent<UnityEngine.UI.RawImage>().texture = CamImage.CameraTexture;
+            this.UpdateAsObservable()
+                .ThrottleFirstFrame(60)
+                .Subscribe(_ => Send())
+                .AddTo(gameObject);
+
             CamImage.Play();
+        }
+
+        /// <summary>
+        /// 送信
+        /// </summary>
+        private void Send()
+        {
+            if (!CamImage.IsPlaying) { return; }
+            var Pixels = CamImage.CameraTexture.GetPixels();
+            byte[] Data = new byte[Pixels.Length * 3];
+            for (var i = 0; i < Pixels.Length; i++)
+            {
+                Data[i * 3 + 0] = (byte)(Pixels[i].r * 255);
+                Data[i * 3 + 1] = (byte)(Pixels[i].g * 255);
+                Data[i * 3 + 2] = (byte)(Pixels[i].b * 255);
+            }
+
+            // ハンドシェイク
+            int[] HandshakeData = new int[] { View.ViewID, CameraImage.TextureWidth, CameraImage.TextureHeight, Data.Length };
+            PhotonNetwork.RaiseEvent(HandshakeEventCode, HandshakeData, new RaiseEventOptions()
+            {
+                CachingOption = EventCaching.DoNotCache,
+                Receivers = ReceiverGroup.All
+            }, new SendOptions()
+            {
+                Reliability = true
+            });
         }
     }
 }
